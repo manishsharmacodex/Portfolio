3fa23c12fb039faf480061d0c69146231b9900bb
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-20 13:11:54 +0530

    ok

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Hey I'm Manish Kumar Sharma</h1>
-      <p>Web Developer & App Developer</p>
+      {/* <h1>Hey I'm Manish Kumar Sharma</h1>
+      <p>Web Developer & App Developer</p> */}
       <div className="header-btn">
         <div>
           <button>Project Manager</button>
